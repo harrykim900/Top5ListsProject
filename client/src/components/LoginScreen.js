9884e2c08cea0d343de60fc6b2cbad8572aacb2e
Harry <harrykim900@yahoo.com>
2021-11-04 13:30:35 -0400

    Removed remember me checkbox from login screen

--- client/src/components/LoginScreen.js
+++ client/src/components/LoginScreen.js
@@ -85,10 +85,6 @@ export default function LoginScreen() {
                             id="password"
                             autoComplete="current-password"
                         />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                        />
                         <Button
                             type="submit"
                             fullWidth
